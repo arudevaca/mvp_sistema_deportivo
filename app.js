d7315ce14564654decf946a47cc1c72ff80096d5
Aruamá Devaca <aruzetav@gmail.com>
2026-08-26 14:53:38 -0300

    Agrega endpoint DELETE /jugadores/:id

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.get('/', (request, response) => {
   response.send("Hola mundo")
 })
 
-const jugadores =
+let jugadores =
 [
   {id: 1, nombre: "Juanito", edad: 15},
   {id: 2, nombre: "José", edad: 16},
@@ -18,7 +18,7 @@ const jugadores =
 app.get('/jugadores',(request, response) => {
   response.json(jugadores)
 })
-// activa el middleware para json, Sin esta línea, no podrías leer lo que el usuario manda en el cuerpo (body) de las peticiones.
+// activa el middleware para json, Sin esta línea, no podría leer lo que el usuario manda en el cuerpo (body) de las peticiones.
 app.use(express.json())
 
 
@@ -30,6 +30,16 @@ app.post('/jugadores',(request, response) =>{
 }
 )
 
+app.delete('/jugadores/:id', (request, response)=> {
+  jugadores = jugadores.filter((jugador) => 
+  //Se chequea cada jugador.id con el id que el usuario quiere borrar, si no coincide devuelve true y se mantiene, si coincide devuelve false se descarta 
+  // jugador siendo el nombre que se le da a los elementos dentro de jugadores
+    jugador.id !== Number(request.params.id)  
+) 
+response.json(jugadores)
+}
+)
+ 
 
 // segunda ruta creada metodo listen, esta app escucha el puerto 1234, y aparece aviso de servidor levantado en...
 app.listen(PORT, () =>{
