34632eb38591e35a0da2317b22cfc1df8be5184e
Aruamá Devaca <aruzetav@gmail.com>
2026-09-01 19:51:29 -0300

    Agrega endpoint PUT /jugadores/:id

--- app.js
+++ app.js
@@ -18,25 +18,38 @@ let jugadores =
 app.get('/jugadores',(request, response) => {
   response.json(jugadores)
 })
+
+
 // activa el middleware para json, Sin esta línea, no podría leer lo que el usuario manda en el cuerpo (body) de las peticiones.
 app.use(express.json())
 
-
-app.post('/jugadores',(request, response) =>{
-  const nuevoId = jugadores.length + 1;   
-  const jugadorNuevo = {id: nuevoId, nombre: request.body.nombre, edad: request.body.edad}
+app.post('/jugadores',(request, response) => {
+  let nuevoId = jugadores.length + 1;   
+  let jugadorNuevo = {id: nuevoId, nombre: request.body.nombre, edad: request.body.edad}
   jugadores.push(jugadorNuevo)
   response.json(jugadorNuevo)
 }
 )
 
-app.delete('/jugadores/:id', (request, response)=> {
+app.delete('/jugadores/:id', (request, response) => {
   jugadores = jugadores.filter((jugador) => 
   //Se chequea cada jugador.id con el id que el usuario quiere borrar, si no coincide devuelve true y se mantiene, si coincide devuelve false se descarta 
   // jugador siendo el nombre que se le da a los elementos dentro de jugadores
     jugador.id !== Number(request.params.id)  
 ) 
 response.json(jugadores)
+}
+)
+
+app.put('/jugadores/:id', (request, response)  => {
+  // Buscar, dentro del array jugadores, el objeto cuyo id coincide con el que vino en la URL — usando find().
+  let jugadorEditado = jugadores.find((jugadorAEditar) =>
+    // Se busca el id a editar con su id existente 
+    jugadorAEditar.id === Number(request.params.id))
+    // Una vez encontrado, se reasignan edad y nombre
+    jugadorEditado.nombre = request.body.nombre
+    jugadorEditado.edad = request.body.edad
+response.json(jugadorEditado)
 }
 )
  
